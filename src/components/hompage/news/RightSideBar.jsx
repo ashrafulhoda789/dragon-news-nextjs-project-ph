c9af81666b37c7270693aae41d5f7226e4d44eca
ashraful-hoda <asrafulhoda789@gmail.com>
2026-05-01 16:17:23 +0600

    handle duplicate login

--- src/components/hompage/news/RightSideBar.jsx
+++ src/components/hompage/news/RightSideBar.jsx
@@ -1,35 +1,56 @@
 'use client'
 import { authClient } from "@/lib/auth-client";
+import { useEffect, useState } from "react";
 import { FaGithub, FaGoogle } from "react-icons/fa";
 
 
 const RightSideBar = () => {
 
+    const [session, setSession] = useState(null);
+
+    useEffect(() => {
+        authClient.getSession().then(res => {
+            setSession(res.data);
+        })
+    }, []);
+
     const handleGoogleSignIn = async () => {
+        if (session) return;
+
         const data = await authClient.signIn.social({
             provider: "google",
         });
         // console.log(data);
     };
 
-    const handleGithubSignIn = async() => {
+    const handleGithubSignIn = async () => {
+        if (session) return;
+
         const data = await authClient.signIn.social({
             provider: "github"
         });
         // console.log(data);
     }
-    
+
     return (
         <div>
-            <h2 className="font-bold text-lg ">Login With</h2>
-            <div className="flex flex-col gap-2 mt-5">
-                <button className="btn" onClick={handleGoogleSignIn}>
-                    <FaGoogle></FaGoogle>
-                    Login With Google</button>
-                <button className="btn" onClick={handleGithubSignIn}>
-                    <FaGithub></FaGithub>
-                    Login With GitHub</button>
-            </div>
+            {
+                session ? (
+                    ""
+                ) : (
+                    <div>
+                        <h2 className="font-bold text-lg ">Login With</h2>
+                        <div className="flex flex-col gap-2 mt-5">
+                            <button className="btn" onClick={handleGoogleSignIn}>
+                                <FaGoogle></FaGoogle>
+                                Login With Google</button>
+                            <button className="btn" onClick={handleGithubSignIn}>
+                                <FaGithub></FaGithub>
+                                Login With GitHub</button>
+                        </div>
+                    </div>
+                )
+            }
         </div>
     );
 };
