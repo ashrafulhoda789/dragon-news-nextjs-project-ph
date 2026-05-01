66fdd248101d9527b61414a5d5c9444ddb204f9f
ashraful-hoda <asrafulhoda789@gmail.com>
2026-05-01 15:33:02 +0600

    socail based sign in added

--- README.md
+++ README.md
@@ -79,6 +79,8 @@ Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/bui
     8. setup for login
     9. Showed user data in Navbar dynamically using useSession()
     10. password see button add and toggle
+    11. add google sign In and Github sign In
+
 
     
  -->
--- src/components/hompage/news/RightSideBar.jsx
+++ src/components/hompage/news/RightSideBar.jsx
@@ -1,15 +1,31 @@
+'use client'
+import { authClient } from "@/lib/auth-client";
 import { FaGithub, FaGoogle } from "react-icons/fa";
 
 
 const RightSideBar = () => {
+
+    const handleGoogleSignIn = async () => {
+        const data = await authClient.signIn.social({
+            provider: "google",
+        });
+        console.log(data);
+    };
+
+    const handleGithubSignIn = async() => {
+        const data = await authClient.signIn.social({
+            provider: "github"
+        });
+        console.log(data);
+    }
     return (
         <div>
             <h2 className="font-bold text-lg ">Login With</h2>
             <div className="flex flex-col gap-2 mt-5">
-                <button className="btn">
+                <button className="btn" onClick={handleGoogleSignIn}>
                     <FaGoogle></FaGoogle>
                     Login With Google</button>
-                <button className="btn">
+                <button className="btn" onClick={handleGithubSignIn}>
                     <FaGithub></FaGithub>
                     Login With GitHub</button>
             </div>
